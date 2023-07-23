0c10d1bc6f5bb426ed479ad2d86f6b93bc778046
Carlos Caceres <ccaceres0461@gmail.com>
2023-07-23 07:04:24 -0400

    Initial project Add config properties Add structure package Add entity

---
+++ src/main/java/com/example/demo/model/Estado.java
@@ -0,0 +1,45 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="estado")
+public class Estado {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;	
+	private String nombre;
+	@ManyToOne
+	@JoinColumn(name="id_pais")
+	private Pais pais;
+	
+	public Estado(Pais pais, String nombre) {
+		super();
+		this.pais = pais;
+		this.nombre = nombre;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
